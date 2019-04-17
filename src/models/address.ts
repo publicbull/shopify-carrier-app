e1ba770f3ac3b7cad73829b48e49e6c590fbee06
Arsen A. Gutsal <a.gutsal@softsky.com.ua>
2019-04-17 20:39:13 +0300

    now all simple fields are indexed for `Address` model

--- src/models/address.ts
+++ src/models/address.ts
@@ -1,11 +1,11 @@
 export const AddressSchema = {
     name: 'Address',
     properties: {
-        barangay: 'string',
-        carrier: 'Carrier',
-        city: 'string',
-        province: 'string',
-        state: 'string',
-        zip: 'string'
+        barangay: { type: 'string', indexed: true },
+        carrier: { type: 'Carrier', indexed: false },
+        city: { type: 'string', indexed: true },
+        province: { type: 'string', indexed: true },
+        state: { type: 'string', indexed: true },
+        zip: { type: 'string', indexed: true }
     }
 }
