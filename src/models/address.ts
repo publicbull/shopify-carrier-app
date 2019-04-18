b4c83aa9457d29883da4350d0941e38c362b3daa
Arsen A. Gutsal <a.gutsal@softsky.com.ua>
2019-04-18 08:34:46 +0300

    linkingObjects:addresses added

--- src/models/address.ts
+++ src/models/address.ts
@@ -2,7 +2,11 @@ export const AddressSchema = {
     name: 'Address',
     properties: {
         barangay: { type: 'string', indexed: true },
-        carrier: { type: 'Carrier', indexed: false },
+        carrier: {
+            objectType: 'Carrier',
+            property: 'addresses',
+            type: 'linkingObjects'
+        },
         city: { type: 'string', indexed: true },
         province: { type: 'string', indexed: true },
         state: { type: 'string', indexed: true },
